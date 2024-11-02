ce0348d523bd63f18866dbc29e6ea649b1b413be
baslhabib <84862641+baslhabib@users.noreply.github.com>
2024-11-02 08:10:47 +0300

    Update app.py

--- genetic_algorithm/app.py
+++ genetic_algorithm/app.py
@@ -4,9 +4,10 @@ from genetic_algorithm import run_genetic_algorithm
 app = Flask(__name__)
 
 @app.route('/run-genetic-algorithm', methods=['GET'])
-def run_algorithm():
-    final_population = run_genetic_algorithm()
-    return jsonify(final_population)
+def run_genetic_algorithm():
+    best_individual, best_fitness = genetic_algorithm()
+    return jsonify({"best_individual": best_individual, "fitness": best_fitness})
+
 
 if __name__ == '__main__':
     app.run(debug=True)
