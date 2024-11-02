c4d98c5411189aff1240a96d988ae26dbb28687f
baslhabib <84862641+baslhabib@users.noreply.github.com>
2024-11-02 08:01:27 +0300

    Update index.jsx

--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -1,4 +1,4 @@
-import GeneticAlgorithmComponent from './GeneticAlgorithmComponent';
+import GeneticAlgorithmComponent from '../GeneticAlgorithmComponent';
 
 export default function Index() {
     return (
